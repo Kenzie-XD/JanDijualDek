408e1dcd474e364fa3ab7af94c545ef1f8f0022f
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-26 09:13:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send('Please connect me into a hosting website to enable 24/7 hosting. ItzNexus#5354')
+  res.send('Please connect me into a hosting website to enable 24/7 hosting.')
 })
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Creator: ItzNexus")
+    console.log("Creator: Kenzie-XD")
   })
 }
 
