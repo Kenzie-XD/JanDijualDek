3838cb3cc30a5f1619e3f3f6ef134af6db01ae97
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-26 09:24:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express")
-
 const server = express()
 
 server.all("/", (req, res) => {
   res.send('Please connect me into a hosting website to enable 24/7 hosting.')
 })
 
-function keepAlive() {
-  server.listen(3000, () => {
-    console.log("Creator: Kenzie-XD")
-  })
+function keepAlive(){
+    server.listen(process.env.PORT, ()=>{console.log("Creator: Kenzie-XD")});
 }
 
 module.exports = keepAlive
